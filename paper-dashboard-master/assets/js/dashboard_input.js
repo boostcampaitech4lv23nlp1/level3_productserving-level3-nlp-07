71b558449245c1853ccccbf31379c0b2a08a01ea
datakim1201 <ssukim1201@gmail.com>
2023-02-05 01:04:00 +0900

    feat:dts_input

--- paper-dashboard-master/assets/js/dashboard_input.js
+++ paper-dashboard-master/assets/js/dashboard_input.js
@@ -6,7 +6,7 @@ slider.addEventListener("input", function() {
   });
 var options = ["Option 1", "Option 2", "Option 3", "Option 4"];
 
-var select = document.getElementById("basicSelect");
+var select = document.getElementById("selchat");
 
 for (var i = 0; i < options.length; i++) {
   var opt = options[i];
@@ -15,6 +15,7 @@ for (var i = 0; i < options.length; i++) {
   el.value = opt;
   select.appendChild(el);
 }
+console.log(select)
 
 const checkboxGroup = document.getElementById('interest-Keyword');
 var data = ['채용', '취업', '코테', '인공지능', 'AI','알고리즘','면접','대기업', 'IT기업', 'IT', 'ML','DL','CNN', 'RNN', 'CV', 'NLP','Recsys']
@@ -24,6 +25,7 @@ for (let i = 0; i < data.length; i++) {
   checkbox.setAttribute('type', 'checkbox');
   checkbox.setAttribute('id', `checkbox${i}`);
   checkbox.setAttribute('style', 'display: inline-block; margin-right: 10px; ');
+  checkbox.setAttribute('value',data[i])
 
   const label = document.createElement('label');
   label.setAttribute('for',`checkbox${i}`);
@@ -36,6 +38,7 @@ const dateInput = document.getElementById("dateInput");
 const today = new Date();
 const todate = document.createElement('input');
 todate.setAttribute('type', 'date');
+todate.setAttribute('id', 'start_date');
 todate.setAttribute('style', 'display: inline-block; width: 300px; height: 50px');
 // Set the maximum date to today's date
 todate.max = today.toISOString().split("T")[0];
